afe2cf25b7d37f41ad468481953b7956861fc790
QuocVan <lyquocvan1999@gmail.com>
2024-05-22 13:26:42 +0700

    cap nhat bt 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
-import { addArr, resetInput } from './method/method.js';
+import { addArr, resetInput, tinhTongSoDuongChuoi } from './method/method.js';
 
 let chuoiSo = [];
 let btnThemSo = document.querySelector('#btnThemSo');
+let btnResetChuoi = document.querySelector('#btnResetChuoi');
 let soN = document.querySelector('#soN');
 let showChuoiSo = document.querySelector('#showChuoiSo');
+
+let btnTinhTong = document.querySelector('#btnTinhTong');
+let kqTinhTong = document.querySelector('#kqTinhTong');
 
 btnThemSo.addEventListener('click', () => {
     let number = Number(soN.value)
@@ -11,4 +15,15 @@ btnThemSo.addEventListener('click', () => {
     addArr(chuoiSo, number);
     showChuoiSo.innerHTML = chuoiSo;
     resetInput(soN);
+});
+
+btnResetChuoi.addEventListener('click', () => {
+    chuoiSo = [];
+    showChuoiSo.innerHTML = chuoiSo;
+});
+
+btnTinhTong.addEventListener('click', () => {
+    let ketQua = tinhTongSoDuongChuoi(chuoiSo);
+
+    kqTinhTong.innerHTML = `Tổng số dương: ${ketQua}`;
 });
--- assets/js/method/method.js
+++ assets/js/method/method.js
@@ -15,3 +15,13 @@ export function addArr(arr, data) {
 export function resetInput(input) {
     input.value = '';
 }
+
+export function tinhTongSoDuongChuoi(arr) {
+    let tong = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            tong += arr[i];
+        }
+    }
+    return tong > 0 ? tong : 0;
+}
